861994827202089add4a8dcdf2f4fb4c98754093
humberto <humberto.gnm98@gmail.com>
2023-09-22 15:03:38 -0300

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+export default function Header() {
+  return (
+    <header className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+      <div className="pb-6 md:pb-0">
+        <h1 className="font-garamond font-semibold text-center md:text-start text-2xl">
+          Contas da <strong className="text-amber-900">Casa</strong>
+        </h1>
+      </div>
+      <div className="flex items-center space-x-5 flex-1 justify-end w-full">
+        <form className="flex items-center space-x-5 bg-white/50 rounded-md p-2 shadow-md flex-1
+        md:flex-initial">
+          <MagnifyingGlassIcon className="h-6 w-6 text-gay-400" />
+          <input
+            type="text"
+            placeholder="Pesquisar"
+            className="flex-1 outline-none p-2 bg-transparent"
+          />
+          <button hidden type="submit">
+            Search
+          </button>
+        </form>
+        <div className="bg-amber-600/30 p-4 rounded-full">
+          <span>SS</span>
+        </div>
+      </div>
+    </header>
+  )
+}
