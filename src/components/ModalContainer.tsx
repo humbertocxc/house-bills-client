3386f088c9e6b5851562e595c5ed27932979df7a
humberto <humberto.gnm98@gmail.com>
2023-09-27 11:03:19 -0300

    create modal container with headlessui

---
+++ src/components/ModalContainer.tsx
@@ -0,0 +1,28 @@
+import { Dialog } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import { ReactNode } from "react";
+
+interface IModalContainer {
+  children: ReactNode,
+  hide: () => void,
+  isOpen: boolean,
+  modalTitle?: string,
+}
+
+export default function ModalContainer({ children, hide, isOpen, modalTitle }: IModalContainer) {
+  return (
+    <Dialog open={isOpen} onClose={hide}>
+      <div className="fixed inset-0 flex w-screen h-screen bg-black/60 items-center justify-center">
+        <Dialog.Panel className="w-full max-w-md p-6 rounded-xl bg-orange-200">
+          <div className="flex justify-between">
+            <Dialog.Title>{modalTitle}</Dialog.Title>
+            <button type="button" className="float-right outline-none" onClick={hide}>
+              <XMarkIcon className="w-4 h-4 m-2" />
+            </button>
+          </div>
+          {children}
+        </Dialog.Panel>
+      </div>
+    </Dialog>
+  )
+}
