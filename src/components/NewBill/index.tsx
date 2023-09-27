8fc8487cedd637506b64570008ae86774ac936e3
humberto <humberto.gnm98@gmail.com>
2023-09-27 16:47:48 -0300

    (feat): Price and Date components used on form

--- src/components/NewBill/index.tsx
+++ src/components/NewBill/index.tsx
@@ -2,9 +2,11 @@ import { Form, Formik } from "formik";
 import { v4 as uuid } from "uuid";
 import BillSchema from "./schema";
 import { defaultValues } from "./defaultValues";
-import TextField from "../TextField";
+import TextField from "../Form/TextField";
 import { createBill } from "../../../utils/createBill";
 import { useBoardStore } from "@/app/store";
+import DateField from "../Form/DateField";
+import PriceInput from "../Form/PriceInput";
 
 interface INewBill {
   columnId: TypedColumn,
@@ -15,8 +17,11 @@ export default function NewBillForm({ columnId, hide }: INewBill) {
   const getBoard = useBoardStore((state) => state.getBoard)
 
   const handleSubmit = (data: BillForm) => {
+    const parsedTotalValue = data.totalValue.toString().replace(/[^0-9]/g, '')
+
     const newBill: Bill = {
       ...data,
+      totalValue: Number(parsedTotalValue),
       $id: uuid(),
       $purchaseDate: new Date("yyyy-mm-dd"),
       status: columnId,
@@ -36,14 +41,14 @@ export default function NewBillForm({ columnId, hide }: INewBill) {
         validationSchema={BillSchema}
         onSubmit={handleSubmit}
       >
-        {({ errors }) => (
+        {({ errors, values }) => (
           <Form className="space-y-2 py-2">
             <TextField autoFocus label="Título da conta" error={errors.title} name="title" type="text" />
             <TextField label="Descrição da conta" error={errors.description} name="description" type="text" />
-            <TextField label="Prazo de pagamento" error={errors.dueDate} name="dueDate" type="date" />
-            <TextField label="Valor total" error={errors.totalValue} name="totalValue" type="number" />
-            <TextField label="Número de parcelas" error={errors.totalInstallments} name="totalInstallments" type="number" />
-            <button className="bg-amber-900/50 hover:bg-amber-900/70 rounded-lg p-2 w-full" type="submit">Adicionar Conta</button>
+            <PriceInput name="totalValue" value={String(values.totalValue)} error={errors.title} />
+            <DateField label="Prazo de pagamento" name="dueDate" />
+            <TextField label="Número de parcelas" error={errors.totalInstallments} name="totalInstallments" type="text" />
+            <button className="bg-amber-900/50 hover:bg-amber-900/70 rounded-lg p-2 w-full shadow-md" type="submit">Adicionar Conta</button>
           </Form>
         )}
       </Formik>
